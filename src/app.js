440809cb51859070b28f460c3bfb108f346b9745
Kara de la Marck <karadelamarck@gmail.com>
2016-07-08 17:40:58 +0100

    Feat: start of add to linkedlist in redis

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
+var redis = require('redis');
 
+var client = redis.createClient();
 
-var obj = {
-  
+var newPost = {
+ date: '1467994412124',
+ text: 'hi #fri',
+ hashtags: '#fri'
 }
+
+
+function addToList(listName, newPost) {
+  var date = newPost.date;
+  client.lpush(listName, date);
+}
+
+addToList('dateIDs', newPost);
