6574ddc2ff1bc599a711113448da894dc0e726c5
mcelearr <rorymcelearney@gmail.com>
2016-07-12 21:08:05 +0100

    adds content to global array then creates it dynamically

--- public/createContent.js
+++ public/createContent.js
@@ -1,10 +1,36 @@
+var globalArray = [];
+
 function createContent(post) {
-  createNode('post-container',post.date);
-  createNode('post-container',post.text);
+  removeElementsByClass("post");
+  globalArray.push(post);
+  globalArray.forEach(function(element, index) {
+    console.log(element);
+    createPostDiv(element.date, element.text);
+  });
 }
 
-function createNode(parent, text) {
-  var newParagraph = document.createElement('p');
-  newParagraph.textContent = text;
-  document.getElementById(parent).appendChild(newParagraph);
+function createPostDiv(date, text) {
+  var post = document.createElement('div');
+  post.className = "post";
+  var dateLine = document.createElement('p');
+  var dateContent = document.createTextNode(date);
+  var textLine = document.createElement('p');
+  var textContent = document.createTextNode(text);
+  post.appendChild(dateLine).appendChild(dateContent);
+  post.appendChild(textLine).appendChild(textContent);
+  document.getElementById('post-container').appendChild(post);
 }
+
+// function appendContentToPost(node, content)
+
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+//Onload go to the database and get back the last ten posts
+//Put them into an array of objects
+
+//When a post is made, add to the array
+//Loop through the array and call createNode() for each element in the array
