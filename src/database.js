5bb36c3cda7e59f06522b458218d1d36f5ccabec
Heather <hdavidson94@gmail.com>
2016-07-14 11:29:33 +0100

    Hide environment variables

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 var pg = require('pg');
 pg.defaults.ssl = true;
+require('env2')('config.env');
 //this connection string will be hidden as environment variables
-var connectionString = 'postgres://offlqpmqxhyutg:oaFSW5OBBnHQkrxVuf6ZU5dNgp@ec2-54-243-55-26.compute-1.amazonaws.com:5432/d23jepi7vhlk8h'
+var connectionString = process.env.DATABASE_URL;
 
 function makePost(post) {
   var client = new pg.Client(connectionString);
