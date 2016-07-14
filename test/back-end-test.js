cd5653db07e1e5f324d1bfbddfb81e6fe1d3d717
mcelearr <rorymcelearney@gmail.com>
2016-07-14 14:55:47 +0100

    adds getPostsByHashtag function

--- test/back-end-test.js
+++ test/back-end-test.js
@@ -2,7 +2,7 @@ const tape = require('tape');
 const shot = require('shot');
 const handler = require('../src/handler.js');
 const makePost = require('../src/makePost.js');
-const getPostById = require('../src/getPost.js');
+const getPost = require('../src/getPost.js');
 
 tape('tests for / endpoint', t => {
   shot.inject(handler, { method: 'get', url: '/' }, (res) => {
@@ -26,7 +26,7 @@ tape('Test post function', t => {
 
   function setPostIdToTest (postid) {
     postIdToTest = postid;
-    getPostById(postIdToTest, function(post){
+    getPost.getPostById(postIdToTest, function(post){
       t.deepEqual(post,
         { date: new Date('Wed Jul 13 2016'),
           hashtags: '#Friday',
@@ -40,23 +40,47 @@ tape('Test post function', t => {
   }
 });
 
+// tape('Test get function', t => {
+//   let postIds = [];
 //
-// function makePost(client, post, cb){
-//   client.get('tweetID', function(err, id) {
-//     if (err) console.log(err);
-//     client.hmset(id, ['date', post.date,'text', post.text, 'hashTags', post.hashtags]);
-//     client.hgetall(id, function(err, obj) {
-//       if(err)console.log(err);
-//       console.log(obj);
-//       return cb(obj)
+//   const fakePostArray = [{
+//     date: 'Wed Jul 13 2016 08:54:44 GMT 0100 (BST)',
+//     text: 'I hope we can get our app working by #Friday',
+//     hashtags: '#Friday'
+//   },{
+//     date: 'Wed Jul 13 2016 08:54:44 GMT 0100 (BST)',
+//     text: 'I hope we can get our app working by #Friday',
+//     hashtags: '#Friday'
+//   },{
+//     date: 'Wed Jul 13 2016 08:54:44 GMT 0100 (BST)',
+//     text: 'I hope we can get our app working by #Friday',
+//     hashtags: '#Friday'
+//   },{
+//     date: 'Wed Jul 13 2016 08:54:44 GMT 0100 (BST)',
+//     text: 'I hope we can get our app working by #Friday',
+//     hashtags: '#Friday'
+//   },{
+//     date: 'Wed Jul 13 2016 08:54:44 GMT 0100 (BST)',
+//     text: 'I hope we can get our app working by #Friday',
+//     hashtags: '#Friday'
+//   }]
+//
+//   fakePostArray.forEach(function(post){
+//     makePost(post, setPostIdsToRetrieve);
+//   });
+//
+//   function setPostIdToTest (postid) {
+//     postIdToTest = postid;
+//     getPostById(postIdToTest, function(post){
+//       t.deepEqual(post,
+//         { date: new Date('Wed Jul 13 2016'),
+//           hashtags: '#Friday',
+//           postid: postIdToTest,
+//           text: 'I hope we can get our app working by #Friday',
+//           time: '08:54:44',
+//           username: null },
+//       'makePost should send a post to the database');
+//       t.end();
 //     });
-//   });
-// }
-
-// function getPost(client, id, cb) {
-//   client.hgetall(id, function(error, reply){
-//     if(error)console.log(error);
-//     console.log(reply);
-//     return cb(reply)
-//   });
-// }
+//   }
+// });
