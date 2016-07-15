17f948ec306d345628d3789c89bfc57cfec3d349
mcelearr <rorymcelearney@gmail.com>
2016-07-15 09:55:53 +0100

    adds search functionality

--- src/handler.js
+++ src/handler.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 const querystring = require('querystring');
 const makePost = require('./makePost.js');
 const createUserRecord = require('./createUserRecord.js')
+const getPost = require('./getPost.js');
 
 function handler(request, response) {
   var endpoint = request.url;
-
   if (endpoint === '/') {
     var pathToIndex = __dirname + '/../public/index.html';
-
     fs.readFile(pathToIndex, function (error, file) {
       if (error) {
         throw error;
@@ -19,11 +18,9 @@ function handler(request, response) {
     });
   } else if (endpoint === '/add-user-record') {
     var data = '';
-
     request.on('data', function (chunk) {
       data += chunk;
     });
-
     request.on('end', function () {
       var newRecord = querystring.parse(data);
       createUserRecord(newRecord, sendUserSuccessResponse);
@@ -38,11 +35,9 @@ function handler(request, response) {
 
   } else if (endpoint === '/add-post') {
     var data = '';
-
     request.on('data', function (chunk) {
       data += chunk;
     });
-
     request.on('end', function () {
       var newPost = querystring.parse(data);
       makePost(newPost, sendPostSuccessResponse);
@@ -55,33 +50,19 @@ function handler(request, response) {
       }
   });
 
-
-
-  } else if (endpoint === '/get-posts') {
-    // var pathToJSON = __dirname + '/posts.json';
-    //
-    // fs.readFile(pathToJSON, function (error, blogposts) {
-    //   if (error) {
-    //     throw error;
-    //   }
-      var fakePosts = [{
-        date: 'Wed Jul 13 2016 08:54:44 GMT 0100 (BST)',
-        text: 'I hope we can get our app working by #Friday',
-        hashTags: '#Friday'
-      },{
-        date: 'Wed Jul 13 2016 08:55:03 GMT 0100 (BST)',
-        text: 'Do a coding bootcamp they said. It will be easy they said...',
-        hashTags: ''
-      }];
+  } else if (endpoint.indexOf('/get-posts') !== -1) {
+    var searchTerm = endpoint.split('=')[1];
+    getPost.getPostBySearchTerm(searchTerm, sendSearchSuccessResponse);
+    function sendSearchSuccessResponse(posts) {
       response.writeHead(200, { 'Content-Type': 'text/json' });
-      response.write(JSON.stringify(fakePosts));
+      response.write(JSON.stringify(posts));
       response.end();
-    // });
+    }
+
   } else {
     var pathToFile = __dirname + '/../public' + endpoint;
     var fileExtensionArray = endpoint.split('.');
     var fileExtension = fileExtensionArray[1];
-
     fs.readFile(pathToFile, function (error, file) {
       if (error) {
         throw error;
